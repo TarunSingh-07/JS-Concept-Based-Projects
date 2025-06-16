00c9353a55f2ab0782799d22e88a99b9143d3266
Tarun Kumar Singh <163575692+TarunSingh-07@users.noreply.github.com>
2025-06-16 22:19:39 +0530

    Update script.js

--- quiz app/script.js
+++ quiz app/script.js
@@ -37,6 +37,7 @@ const questions = [
     }
 ];
 
+
 const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
